2ed9e1a6d70e20e121feba08b0814595e7a32bfa
steve.loveless <steve.loveless@69afb7d0-a71d-11de-8c46-7bd654735883>
2009-09-22 02:22:43 +0000

    initial import

---
+++ trunk/itunes_rem_dupes.rb
@@ -0,0 +1,278 @@
+# probably should actually only allow this at the iTunes Library level,
+# where i store each child in an array (=artist), then act on subdirs.  Each
+# subdir = album, which is stored in another array.  Get the list of files in
+# the album into another array/hash, where each element points to the md5sum
+# generated for that song.  Next compare md5 sums.  If md5sums are ==, check for
+# the file with the " 1" and offer to delete it.
+
+require 'digest/md5'
+require 'fileutils'
+require 'logger'
+
+#------------------------------------------------------------------------------
+# SCRIPT SETTINGS:
+# set itunes dir--default is ~/Music/iTunes/
+#@itunes_base = "/Users/sloveless/tmp/iTunes test/"
+@itunes_base = "/Volumes/MEDIA/Music/"
+
+# get iTunes Music dir--default is ~/Music/iTunes/iTunes Music/
+# this dir contains the [Artist] directories
+@itunes_music = "#{@itunes_base}/iTunes/"
+
+# set log file
+@log_file = ENV['HOME']+"/Desktop/itunes_rem_dupes_log.txt"
+
+# choose to use "trash" or not
+# enabling this means moving files to a local folder for review so the user
+# can choose to delete after reviewing.
+# TODO:  FALSE = DOESN'T WORK YET
+@use_trash = true
+@trash_base_dir = ENV['HOME']+"/Music/iTunesTrash"
+
+# choose to auto-accept deletion.
+# enabling this means that instead of prompting to delete the list of found
+# matches, the script will just accept, delete, and move to the next.
+# TODO:  NOT YET IMPLEMENTED
+@auto_accept = false
+
+#------------------------------------------------------------------------------
+
+# init logger
+@logger = Logger.new(@log_file)
+
+# init some global vars
+@artist_dir = ""
+@album_dir = ""
+@matches = Hash.new
+
+# Start by logging trash dir
+puts "Trash dir: #{@trash_base_dir}"
+@logger.debug "Trash dir: #{@trash_base_dir}"
+
+
+# for each subdir (artist dir) in itunes dir...
+Dir.chdir(@itunes_music)
+puts "iTunes dir: #{Dir.pwd}"
+@logger.debug "iTunes dir: #{Dir.pwd}"
+
+#------------------------------------------------------------------------------
+# method:     parse_artists_in
+#------------------------------------------------------------------------------
+def parse_artists_in itunes_music
+  # Go through each artist
+  Dir.foreach(itunes_music) do |artist_dir|
+    # If '.' or '..' or some file, skip
+    if artist_dir.eql? "." or artist_dir.eql? ".." or File.stat(artist_dir).file?
+      next
+    end
+    
+    # Get for later use with trash
+    if @use_trash == true
+      @artist_dir = artist_dir
+    end
+    
+    # Format output
+    80.times {print "#"}
+    puts ""
+    @logger.debug ""
+    puts " Artist:          #{artist_dir}"
+    @logger.debug " Artist:          #{artist_dir}"
+    
+    # Make sure to use the full path
+    artist_dir = File.expand_path artist_dir
+    
+    # change to the artist dir
+    Dir.chdir(artist_dir)
+    puts " Artist dir:     #{Dir.pwd}"
+    @logger.debug " Artist dir:     #{Dir.pwd}"
+
+    # Check out the albums in this artist directory
+    parse_albums_in artist_dir
+    
+    # change back to the itunes music dir
+    Dir.chdir(itunes_music)
+    80.times { print "#" }
+    puts ""
+    @logger.debug ""
+  end
+end
+
+#------------------------------------------------------------------------------
+# method:     parse_albums_in
+#------------------------------------------------------------------------------
+def parse_albums_in artist_dir
+  # Go through each album
+  Dir.foreach(artist_dir) do |album_dir|
+    # If '.' or '..' or some file, skip
+    if album_dir.eql? "." or album_dir.eql? ".." or File.stat(album_dir).file?
+      next
+    end
+    
+    # Get for later use with trash
+    if @use_trash == true
+      @album_dir = album_dir
+    end
+    
+    # Format output
+    80.times {print "-"}
+    puts ""
+    @logger.debug ""
+    puts " Album:          #{album_dir}"
+    @logger.debug " Album:          #{album_dir}"
+    
+    # Make sure we're using the full path
+    album_dir = File.expand_path album_dir
+    Dir.chdir album_dir
+    puts " Album dir:      #{Dir.pwd}"
+    @logger.debug " Album dir:      #{Dir.pwd}"
+    
+    # Check out songs in this album directory
+    parse_songs_in album_dir
+    
+    # Go back to artist dir   to prep for next iteration
+    Dir.chdir artist_dir
+  end
+end
+
+#------------------------------------------------------------------------------
+# method:     parse_song_in
+#------------------------------------------------------------------------------
+def parse_songs_in album_dir
+  # hash to store song/md5sum pairs to
+  songs = Hash.new
+  # hash to store song matches
+  tmp_matches = Array.new
+  
+  # Go through all songs and get md5sum
+  Dir.foreach(album_dir) do |song|
+    # If '.' or '..' or some directory, skip
+    if song.eql? "." or song.eql? ".." or File.stat(song).directory? : next end
+    
+    # Get the md5sum for the file
+    md5sum = Digest::MD5.file(song).hexdigest
+    
+    # Associate the song and md5sum
+    songs["#{song}"] = md5sum
+  end
+  
+  # Sort the songs
+  songs = songs.sort
+  
+  # Print song list
+  songs.each do |key,value|
+    puts " Song:           #{key}"
+    @logger.debug " Song:           #{key}"
+  end
+  
+  songs_to_compare = songs
+  
+  # Compare songs in the album to find duplicates
+  songs.each do |key, value|
+    #puts " #{key} is #{value}"
+    
+    curr_song = key
+    curr_hash = value
+    
+    # Compare current song to other songs in the directory
+    latest_match = songs_to_compare.select do |key, value|
+      # Skip if comparing the same song
+      if key.eql? curr_song : next end
+      
+      # Skip if the song name (curr_key) is > the match found
+      if curr_song.length > key.length : next end
+      
+      # Get values if they're the same as the song in question
+      #puts "Comparing '#{curr_song}' to #{key}"
+      
+      # Check to see if the md5sums are the same
+      value.eql? curr_hash
+    end
+    tmp_matches.push(latest_match)
+    
+  end
+  
+  # Print list of matches
+  @matches = Hash[*tmp_matches.flatten]
+  @matches = @matches.sort
+  
+  # Continue if no matches
+  if @matches.empty?
+  else 
+    puts " Matches:"
+    @logger.debug " Matches:"
+    @matches.each do |key,value|
+      puts "        #{key}"
+      @logger.debug "       #{key}"
+      #puts "#{curr_song} matches #{key}"
+    end 
+    puts ""
+    @logger.debug ""
+    
+    # Check to make sure we want to go ahead with the deleting
+    if proceed?
+      # Create the trash dir if it doesn't exist
+      if File.exists? @trash_base_dir
+      else 
+        puts "Making trash dir"
+        @logger.debug "Making trash dir"
+        Dir.mkdir @trash_base_dir
+      end
+      
+      # Create the artist dir in the trash if it doesn't exist
+      trash_artist_dir = @trash_base_dir + '/' + @artist_dir
+      if File.exists? trash_artist_dir
+      else 
+        puts "Making artist dir in trash"
+        @logger.debug "Making artist dir in trash"
+        Dir.mkdir trash_artist_dir
+      end
+      
+      # Create the album dir in the trash if it doesn't exist
+      trash_album_dir = trash_artist_dir + '/' + @album_dir
+      if File.exists? trash_album_dir
+      else 
+        puts "Making album dir in trash"
+        @logger.debug "Making album dir in trash"
+        Dir.mkdir trash_album_dir
+      end
+      
+      @matches.each do |key, value|
+        # Prep for moving to trash, if enabled 
+        if @use_trash == true
+          puts " Moving '#{key}' to '#{trash_album_dir}'..."
+          @logger.debug " Moving '#{key}' to '#{trash_album_dir}'..."
+          FileUtils.move(key,trash_album_dir)
+        end
+        #File.delete(match)
+      end
+    end
+  end
+end
+
+
+def proceed?
+  good_response = false
+    
+  # Make sure we get a response
+  while good_response == false
+    puts "Trash these files?"
+    $stdout.flush
+    response = $stdin.gets
+    exit if response == nil
+    response.chomp!
+  
+    if response == 'y' or response == 'yes'
+      return true
+    elsif response == 'n' or response == 'no'
+      puts "Quitting."
+      return false
+    elsif response == 'q' or response == 'quit'
+      exit 0
+    else
+      $stderr.puts "Please answer 'yes', 'no' or 'quit'."
+    end
+  end
+end
+
+
+parse_artists_in @itunes_music
